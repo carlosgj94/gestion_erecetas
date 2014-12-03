4c3b4c000d99e4d983e7ccc201e3fe44b81fe8bc
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2014-12-03 23:43:48 +0100

    Archivos estaticos solucionados y empezando el login de usuarios

--- gestion/models.py
+++ gestion/models.py
@@ -1,8 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-
 
 class Medico(models.Model):
     """
@@ -12,6 +11,7 @@ class Medico(models.Model):
     age= models.DateField()
     hospital = models.CharField(max_length=50)
     especialidad = models.CharField(max_length=50)
+    user = models.ForeignKey(User, null=True,on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nombre
@@ -26,6 +26,7 @@ class Farmaceutico(models.Model):
     nombre= models.CharField(max_length=50)
     age= models.DateField()
     calleDeFarmacia= models.CharField(max_length=100)
+    user = models.ForeignKey(User, null=True,on_delete=models.PROTECT)
     def __str__(self):
         return self.nombre
 
--- gestion/views.py
+++ gestion/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required
 class IndexView(View):
     def get(self, request):
         context = {
         }
-        return render(request, 'index.html', context)
+        return render(request, 'gestion/index.html', context)
+
+class LoginView(View):
+    def get(self, request):
+        context = {
+        }
+        return render(request, 'gestion/login.html', context)
+    def post(self, request):
+        print(request.POST)
+
